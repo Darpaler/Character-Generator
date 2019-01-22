83b94eb8e12a429ab93062ff11d355388e9aa3c5
DARIN PALERMO <DARPALER@uat.edu>
2019-01-22 12:15:52 -0700

    Started adding comments.

--- Character Generator/Character.h
+++ Character Generator/Character.h
@@ -11,42 +11,48 @@ using namespace std;
 
 class Character {
 public:
-	Character();
-	Character(std::string N, int H);
-	~Character();
-	
+	//Constructors and Destructors
+	Character();						//Default Constructor
+	Character(std::string N, int H);	//Constructor with Parameters
+	~Character();						//Destructor
+
+	//Getters and Setters
 	void setName(string N);
 	string getName();
 
 	void setHealth(int H);
 	int getHealth();
 
+	//Display
 	void display();
 
 protected:
-	string name;
-	int health = 1;
+	string name;		//The Character's Name
+	int health = 1;		//The Character's Health
 };
 
 class Goblin : public Character
 {
 public:
-	Goblin();
-	Goblin(string N, int H, int M, int C);
-	~Goblin();
+	//Constructors and Destructors
+	Goblin();								//Default Constructor
+	Goblin(string N, int H, int M, int C);	//Constructor with Parameters
+	~Goblin();								//Destructor
 
+	//Getters and Setters
 	int getMischief();
 	void setMischief(int M);
 	
 	int getCombatStrength();
 	void setCombatStrength(int C);
-	
-	void display();
-	void display(bool details);
+
+	//Display
+	void display();					//Default Display
+	void display(bool details);		//Display with Polymorphism
 
 private:
-	int mischief;
-	int combatStrength;
+	int mischief;			//The Character's Tendency Towards Mischief
+	int combatStrength;		//The Character's Combat Strength
 
 };
 
